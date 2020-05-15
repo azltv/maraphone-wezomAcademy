3863120f08ad779eaf1ffb8015ae2de95ac43c50
Anton Zolotov <zolotov@trafficdna.com>
2020-05-16 02:14:48 +0300

    edited models.py (add classes Store, Order, OrderProduct) 02v34:02

--- backend/prj/market/models.py
+++ backend/prj/market/models.py
@@ -22,3 +22,27 @@ class Product(models.Model):
     name = models.CharField(max_length=250, default='')
     image = models.ImageField(upload_to='product')
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+
+class Store(models.Model):
+    provider = models.ForeignKey(Provider, on_delete=models.CASCADE, null=True, blank=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True, blank=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+
+class Order(models.Model):
+
+    STATUS = (
+        ('new', 'new order'),
+        ('pending', 'pending order'),
+        ('finished', 'finished order')
+    )
+
+    consumer = models.ForeignKey(Consumer, on_delete=models.CASCADE, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10, default='new', choices=STATUS)
+
+class OrderProduct(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, null=True, blank=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True, blank=True)
+    ammount = models.IntegerField(default=0)
+
